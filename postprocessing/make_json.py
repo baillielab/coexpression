0a470a562273f7e5894d8da4992af6efcf2cc939
kennethbaillie <bitbucket@baillielab.net>
2019-02-27 16:02:49 +0000

    add POSTPROCESSING add JSON file maker

---
+++ postprocessing/make_json.py
@@ -0,0 +1,74 @@
+﻿''' MAKE A JSON FILE SUITABLE FOR READING BY D3 NETWORK BROWSER'''
+
+#========================
+#=======  JSON  =========
+#========================
+
+import os
+import json
+import networkx as nx
+from networkx.readwrite import json_graph
+#-----------------------------
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-wd', '--workingdirectory',    help='full coexpression results directory')
+args = parser.parse_args()
+#-----------------------------
+def write_json(edgelist, outputfile, nodenames={}, theseclasses={}):
+	G=nx.Graph()
+	for i, edge in enumerate(edgelist):
+		G.add_edge(edge[0],edge[1],weight=float(edge[2])) # specify edge data
+	#put in a blank edge-free node for each one that is missing
+	allnodes = list(G.nodes(data=False))
+	if len(allnodes)>0:
+		for missing in set(range(max(allnodes)+1))-set(allnodes):
+			G.add_node(missing)
+		for n in G:
+			G.node[n]['nodesize'] = 5000
+			try:
+				G.node[n]['name'] = nodenames[n]
+			except:
+				G.node[n]['name'] = 'none'
+			try:
+				G.node[n]['group'] = theseclasses[nodenames[n]]
+			except:
+				G.node[n]['group'] = 'none'
+	d = json_graph.node_link_data(G)
+	jo = open(outputfile,'w')
+	jo.write('<? header("Access-Control-Allow-Origin: https://baillielab.net") ?>\n')
+	json.dump(d, jo)
+	jo.close()
+#-----------------------------
+indscoresfile = os.path.join(args.workingdirectory, "coex.individual_scores")
+f=open(indscoresfile)
+lines = [x.strip().split('\t') for x in f.readlines()]
+f.close()
+sig = {x[0].split(" ")[0]:(float(x[-1])<=0.05)*3 for x in lines[1:]}
+indices = {x[0].split(" ")[0]:i for i,x in enumerate(lines[1:])}
+namesofnodes = {i:x[0].split(" ")[0] for i,x in enumerate(lines[1:])}
+promids = {x[2]:x[0].split(" ")[0] for x in lines[1:]}
+
+networkfile = os.path.join(args.workingdirectory, "coex.network.php")
+f=open(networkfile)
+lines = [x.strip().split('\t') for x in f.readlines()]
+f.close()
+edges=[]
+for line in lines:
+	try:
+		promids[line[0]]
+	except:
+		continue
+	try:
+		promids[line[1]]
+	except:
+		continue
+	if float(line[2]) > 0:
+		edges.append([indices[promids[line[0]]],indices[promids[line[1]]],float(line[2])])
+jsonout = os.path.join(args.workingdirectory, "hitsnetwork.php")
+write_json(edges, jsonout, nodenames=namesofnodes, theseclasses=sig)
+
+
+
+
+
+
---
+++ postprocessing/postprocess_all.py
@@ -0,0 +1,20 @@
+﻿''' RUN ALL POST PROCESSING SCRIPTS OVER A SINGLE RESULTS WORKING DIRECTORY '''
+
+import os
+import subprocess
+import ConfigParser
+#-----------------------------
+scriptpath = os.path.dirname(os.path.realpath(__file__))
+#-----------------------------
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-wd', '--workingdirectory',    help='full coexpression results directory')
+parser.add_argument('-cc', '--config_file', default=os.path.join(scriptpath, '../app.cfg'), help='use a custom config file instead of app.cfg')
+args = parser.parse_args()
+#-----------------------------
+config = ConfigParser.RawConfigParser(allow_no_value=True)
+config.read(args.config_file)
+pathtopython = config.get('directorypaths', 'pathtopython')
+#-----------------------------
+# run all of the following scripts on this directory
+subprocess.call("{} {} -wd {}".format(pathtopython, os.path.join(scriptpath, "make_json.py"), args.workingdirectory), shell=True)
